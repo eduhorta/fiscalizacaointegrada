95f444daf07b764b0e09b7403857a6ac81995b64
Eduardo Horta <eduhorta@hotmail.com>
2022-12-28 12:48:43 -0300

    final doc - start ui/ux

--- main.js
+++ main.js
@@ -13,11 +13,17 @@ function preencher() {
     const matricula2 = document.getElementById('matricula2').value
     const data = document.getElementById('data').value
     const cep = document.getElementById('cep').value
+    const comando = document.getElementById('comando').value
+    const gbm = document.getElementById('gbm').value
+    const chefe = document.getElementById('chefe').value
+    const nota = document.getElementById('nota').value
     document.getElementById('razaoD').innerHTML = razao;
     //razaoD1 - no processo de instauração
     document.getElementById('razaoD1').innerHTML = razao;
     //razaoD2 - no inicio do termo de notificação
     document.getElementById('razaoD2').innerHTML = razao;
+    //razaoD3 - nota p/ BIO
+    document.getElementById('razaoD3').innerHTML = razao;
     document.getElementById('cnpjD').innerHTML = cnpj;
     //cnpjD2 - no inicio do termo de notificação
     document.getElementById('cnpjD2').innerHTML = cnpj;
@@ -26,12 +32,18 @@ function preencher() {
     document.getElementById('enderecoD1').innerHTML = endereco;
     //enderecoD2 - no termo de notificação
     document.getElementById('enderecoD2').innerHTML = endereco;
+    //enderecoD3 - nota p/ BIO
+    document.getElementById('enderecoD3').innerHTML = endereco;
     document.getElementById('numeroD').innerHTML = numero;
     //numeroD1 - no processo de instauração
     document.getElementById('numeroD1').innerHTML = numero;
+    //numeroD2 - nota p/ BIO
+    document.getElementById('numeroD2').innerHTML = numero;
     document.getElementById('bairroD').innerHTML = bairro;
     //bairroD1 - no processo de instauração    
-    document.getElementById('bairroD1').innerHTML = bairro;    
+    document.getElementById('bairroD1').innerHTML = bairro; 
+    //bairroD2 - nota p/ BIO   
+    document.getElementById('bairroD2').innerHTML = bairro;    
     document.getElementById('cidadeD').innerHTML = cidade;
     //CidadeD1 ao final do termo de infração D1
     document.getElementById('cidadeD1').innerHTML = cidade;
@@ -40,11 +52,17 @@ function preencher() {
     document.getElementById('cidadeD3').innerHTML = cidade;
     //cidadeD4 - ao final do termo de notificação
     document.getElementById('cidadeD4').innerHTML = cidade;
+    //cidadeD5 - nota p/ BIO 
+    document.getElementById('cidadeD5').innerHTML = cidade;
     //cepD - processo de instauração
     document.getElementById('cepD').innerHTML = cep;
+    //cepD1 - nota p/ BIO
+    document.getElementById('cepD1').innerHTML = cep;
     document.getElementById('infracaoD').innerHTML = infracao;
     //infracaoD1 - no initicio do termo de instauração
     document.getElementById('infracaoD1').innerHTML = infracao;
+    //infracaoD2 - nota P/ BIO
+    document.getElementById('infracaoD2').innerHTML = infracao;
     document.getElementById('notificacaoD').innerHTML = notificacao;
     //notificacaoD1 - no inicio do termo de notificação
     document.getElementById('notificacaoD1').innerHTML = notificacao;
@@ -53,6 +71,8 @@ function preencher() {
     document.getElementById('vistoriador1D1').innerHTML = vistoriador1;
     //vistoriador1D2 no termo de notificação
     document.getElementById('vistoriador1D2').innerHTML = vistoriador1;
+    //vistoriador1D3 na nota para BIO
+    document.getElementById('vistoriador1D3').innerHTML = vistoriador1;
     document.getElementById('vistoriador2D').innerHTML = vistoriador2;
     //vistoriador2D1 no termo de notificação
     document.getElementById('vistoriador2D1').innerHTML = vistoriador2;
@@ -61,6 +81,8 @@ function preencher() {
     document.getElementById('matricula1D1').innerHTML = matricula1;
     //Matrícula1D2 - vistoriador no termo de notificação
     document.getElementById('matricula1D2').innerHTML = matricula1;
+    //Matricula1D3 - nota p/ BIO
+    document.getElementById('matricula1D3').innerHTML = matricula1;
     document.getElementById('matricula2D').innerHTML = matricula2;
     //matricula2D1 - no termo de noficação
     document.getElementById('matricula2D1').innerHTML = matricula2;
@@ -69,6 +91,25 @@ function preencher() {
     document.getElementById('dataD1').innerHTML = data;
     //dataD2 - no termo de notificação
     document.getElementById('dataD2').innerHTML = data;
+    //dataD3 - nota p/BIO
+    document.getElementById('dataD3').innerHTML = data;
+    //comandoD - auto de infração
+    document.getElementById('comandoD').innerHTML = comando;
+    //comandoD1 - termo de instauração
+    document.getElementById('comandoD1').innerHTML = comando;
+    //chefeD - nota p/ BIO
+    document.getElementById('chefeD').innerHTML = chefe;
+    //gbmD - auto de infração
+    document.getElementById('gbmD').innerHTML = gbm;
+    //gbmD1, D2 e D3 - termo de instauração
+    document.getElementById('gbmD1').innerHTML = gbm;    
+    document.getElementById('gbmD2').innerHTML = gbm;
+    document.getElementById('gbmD3').innerHTML = gbm;
+    //gbmD4 - termo de notificação
+    document.getElementById('gbmD4').innerHTML = gbm;
+    //gbmD5 e D6 - nota p/ BIO
+    document.getElementById('gbmD5').innerHTML = gbm;
+    document.getElementById('gbmD6').innerHTML = gbm;
 }
 
 function Export2Doc(element, filename = '') {
